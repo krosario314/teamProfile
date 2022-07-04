33c7f6ec52d87e66f572ae4627ca40cc1f825107
Kayla Rosario <rosariok314@gmail.com>
2022-07-04 11:33:15 -0400

    changed const to class, employee test js modified

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,7 +1,21 @@
-const Employee {
+class Employee {
   constructor (name, id, email) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
+      this.name = name;
+      this.id = id;
+      this.email = email;
+  }
+  getName() {
+      return this.name;
+  }
+  getId() {
+      return this.id;
+  }
+  getEmail() {
+      return this.email;
+  }
+  getPosition() {
+      return "Employee";
   }
 }
+
+module.exports = Employee;
