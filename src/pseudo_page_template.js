eb6f3c437247c7af0e18036ddef7904697619902
Kayla Rosario <rosariok314@gmail.com>
2022-07-04 12:35:04 -0400

    html temp modified added html elemtns

--- src/pseudo_page_template.js
+++ src/pseudo_page_template.js
@@ -3,6 +3,34 @@
 ***********************************************************/
 
 // Step 1. export function to generate entire html page with template literal
+const generateHTML = function (tString) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Team Portfolio</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+  </style>
+  </head>
+  <body>
+  <div class="header">
+  <div class="jumbotron bg-warning">
+     <h1 class="display-4 text-white text-center">My Employee Profile!</h1>
+  </div>
+  </div>
+  <div class="container-body container-fluid">
+     <div class="row">
+          ${tString} 
+      </div>
+  </div>
+  <script src="https://kit.fontawesome.com/04e4a42510.js" crossorigin="anonymous"></script>  
+  </body>
+  </html>`
+
 // Step 2. function to create team template
 // Step 3. functions to generate manager, engineer, and intern templates
 
