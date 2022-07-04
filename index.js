082b844eb3f00851d838c7b96b2923aa6c44763b
Kayla Rosario <rosariok314@gmail.com>
2022-07-03 21:42:54 -0400

    loggin new managers with office number, new interns with school name and engineer id

--- index.js
+++ index.js
@@ -43,37 +43,81 @@ async function prompt() {
   do {
     try {
       console.log("----------------------------------------");
-      let respond = await inquirer.prompt([
-        {
+        let respond0 = await inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "Enter the name of your employee. ",
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "Enter your employee's ID. ",
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Enter your employee's email address. ",
+          },
+          {
+            type: "input",
+            name: "position",
+            message: "Enter employee's position. ",
+            choices: [
+              "Engineer",
+              "Intern",
+              "Manager"
+            ]
+          }
+        ]);
+        let respond1 = ""
+        if (respond0.role === "Engineer") {
+          respond1 = await inquirer.prompt([{
+              type: "input",
+              name: "github",
+              message: "Enter employee's GitHub username. ",
+          }, ]);
+
+          const engineer = new Engineer(respond0.name, respond0.id, respond0.email, respond1lii.github);
+          tArray.push(engineer);
+          console.log(tArray)
+        } 
+      } catch {if (respond0.role === "Manager") {
+        respond1 = await inquirer.prompt([{
           type: "input",
-          name: "name",
-          message: "Enter the name of your employee.",
-        },
-        {
+          name: "officeNumber",
+          message: "Enter employee's office number. ",
+        }, ]);
+
+        const manager = new Manager( respond0.name, respond0.id, respond0.email, respond1.officeNumber);
+        tArray.push(manager);
+        console.log(tArray)
+      } else if (respond0.role === "Intern") {
+        respond1 = await inquirer.prompt([{
           type: "input",
-          name: "id",
-          message: "Enter your employee's ID.",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "Enter your employee's email address.",
-        },
-        {
-          type: "input",
-          name: "position",
-          message: "Enter employee's position.",
-          choices: [
-            "Engineer",
-            "Intern",
-            "Manager"
-          ]
-        }
-      ]);
-      
+          name: "school",
+          message: "Enter employee's institution. ",
+        }, ]);
+
+        const intern = new Intern( respond0.name, respond0.id, respond0.email, respond1.school);
+        tArray.push(intern);
+        console.log(tArray)
+      }
+    } while (err) {
+      return console.log(err);
     }
-  }
+    respondDone = await inquirer.prompt([{
+      type: "list",
+      name: "finish",
+      message: "Would you like to continue? ",
+      choices: [
+        "Yes",
+        "No"
+      ]
+    },]);
+  } while (respondDone.finish === "Yes");
 }
+
 // Create variables for the output folder and the html file name
 
 // Create an empty array to store the team member objects
