3c42cde5c47b6d82a05060c3c2f3e29b7095155e
Kayla Rosario <rosariok314@gmail.com>
2022-07-12 19:07:04 -0400

    added styling to my html

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-// required
-const Manager = require("./lib/manager"); // proper tagging lns 2-14
-
-const Engineer = require("./lib/engineer");
-
-const Intern = require("./lib/intern");
-
+// applications required
 const inquirer = require("inquirer");
-
 const fs = require("fs");
-
 const util = require("util");
-
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 const html = require("./src/htmlTemplate");
 
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -20,119 +14,130 @@ let tArray = [];
 let tString = ``;
 
 console.clear();
-console.log("----------------------------------------");
-console.log("Team Profile Generator!");
+console.log("---------------------------------------------");
+console.log("Team Portfolio Generator")
 
+// run app
 async function main() {
-  try {
-    await prompt();
-    for (let i = 0; i < tArray.length; i++) {
-      tString = tString + html.generateCard(tArray[i]);
+    try {
+        await prompt()
+
+        for (let i = 0; i < tArray.length; i++) {
+            tString = tString + html.generateCard(tArray[i]);
+        }
+
+        let finalHtml = html.generateHTML(tString)
+
+        console.clear();
+        console.log("---------------------------------------------");
+        console.log("Generating index.html file....");
+        console.log("---------------------------------------------");
+
+        writeFileAsync("index.html", finalHtml);
+
+        console.clear();
+        console.log("---------------------------------------------");
+        console.log("index.html file created successfully");
+        console.log("---------------------------------------------");
+
+    } catch (err) {
+        return console.log(err);
     }
-    let resultHtml = html.generateHTML(tString);
-    console.clear();
-    console.log("----------------------------------------");
-    console.log("index.html file created");
-    console.log("----------------------------------------");
-  } catch (err) {
-    return console.log(err);
-  }
 }
+
+// Inquirer prompts to collect user data
 async function prompt() {
-  let respondDone = "";
-  try {
-    console.log("----------------------------------------");
-    let respond0 = await inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Enter the name of your employee:",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "Enter your employee's ID:",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Enter your employee's email address:",
-      },
-      {
-        type: "list",
-        name: "position",
-        message: "Enter employee's position:",
-        choices: ["Engineer", "Intern", "Manager"],
-      },
-    ]);
-    let respond1 = "";
-    if (respond0.position === "Engineer") {
-      respond1 = await inquirer.prompt([
-        {
-          type: "input",
-          name: "github",
-          message: "Enter employee's GitHub username.",
-        },
-      ]);
+    let responseDone = "";
 
-      const engineer = new Engineer(
-        respond0.name,
-        respond0.id,
-        respond0.email,
-        respond1lii.github
-      );
-      tArray.push(engineer);
-      console.log(tArray);
-    }
-    if (respond0.position === "Manager") {
-      respond1 = await inquirer.prompt([
-        {
-          type: "input",
-          name: "officeNumber",
-          message: "Enter employee's office number:",
-        },
-      ]);
+    do {
+        try {
+            console.log("---------------------------------------------");
+            let response = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is employee's name?",
+                    
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "Enter employee's ID: ",
+                    
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "Enter employee's email address: ",
+                    
+                },
+                {
+                    type: "list",
+                    name: "role",
+                    message: "What is the employee's role: ",
+                    choices: [
+                        "Engineer",
+                        "Intern",
+                        "Manager"
+                    ]
+                }
+            ]);
 
-      const manager = new Manager(
-        respond0.name,
-        respond0.id,
-        respond0.email,
-        respond1.officeNumber
-      );
-      tArray.push(manager);
-      console.log(tArray);
-    } else if (respond0.position === "Intern") {
-      respond1 = await inquirer.prompt([
-        {
-          type: "input",
-          name: "school",
-          message: "Enter employee's institution:",
-        },
-      ]);
+            let response2 = ""
 
-      const intern = new Intern(
-        respond0.name,
-        respond0.id,
-        respond0.email,
-        respond1.school
-      );
-      tArray.push(intern);
-      console.log(tArray);
-    }
-  } catch {
-    (err) => {
-      return console.log(err);
-    };
-    respondDone = await inquirer.prompt([
-      {
-        type: "list",
-        name: "finish",
-        message: "Would you like to continue? ",
-        choices: ["Yes", "No"],
-      },
-    ]);
-  } finally {
-    respondDone.finish === "No";
-  }
+            if (response.role === "Engineer") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "github",
+                    message: "What is the employee's github username?: ",
+                    
+                }, ]);
+
+                // add to tArray
+                const engineer = new Engineer(response.name, response.id, response.email, response2.github);
+                tArray.push(engineer);
+                console.log(tArray)
+            
+            } else if (response.role === "Manager") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "officeNumber",
+                    message: "What is the employee's office number?: ",
+                    
+                }, ]);
+
+                // add to tArray
+                const manager = new Manager( response.name, response.id, response.email, response2.officeNumber);
+                tArray.push(manager);
+                console.log(tArray)
+
+            } else if (response.role === "Intern") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "school",
+                    message: "What school is employee attending: ",
+                   
+                }, ]);
+
+                // add to tArray
+                const intern = new Intern( response.name, response.id, response.email, response2.school);
+                tArray.push(intern);
+                console.log(tArray)
+            }
+        } catch (err) {
+            return console.log(err);
+        }
+        responseDone = await inquirer.prompt([{
+            type: "list",
+            name: "finish",
+            message: "Do you want to continue?: ",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        },]);
+    } while (responseDone.finish === "Yes");
 }
+
+// call main
 main();
--- src/htmlTemplate.js
+++ src/htmlTemplate.js
@@ -1,61 +1,64 @@
 const generateHTML = function (tString) {
   return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Team Portfolio</title>
-  <link href="https://kit.fontawesome.com/04e4a42510.js/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://kit.fontawesome.com/04e4a42510.js/css/font-awesome.min.css" rel="stylesheet">
-  <style>
-  </style>
-  </head>
-  <body>
-  <div class="header">
-  <div class="jumbotron bg-warning">
-     <h1 class="display-4 text-white text-center">My Team Profile!</h1>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Team Portfolio</title>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+</style>
+</head>
+<body>
+<div class="header">
+<div class="jumbotron bg-warning">
+ <h1 class="display-4 text-white text-center">My Team Profile</h1>
+</div>
+</div>
+<div class="container-body container-fluid">
+ <div class="row">
+      ${tString} 
   </div>
-  </div>
-  <div class="container-body container-fluid">
-     <div class="row">
-          ${tString} 
-      </div>
-  </div>
-  <script src="https://kit.fontawesome.com/04e4a42510.js" crossorigin="anonymous"></script>  
-  </body>
-  </html>`
+</div>
+<script src="https://kit.fontawesome.com/04e4a42510.js" crossorigin="anonymous"></script>  
+</body>
+</html>`
+
 }
 
-// Employee cards
+// Generates cards for each employee class based
 const generateCard = function (arr) {
+  //Fontawesome Icons change based on role
   let positionIcon;
-  let positionInfo;
+  //display info
+  let roleInfo;
 
   if (arr.getPosition() === "Manager") {
       positionIcon = `<i class="fas fa-mug-hot"></i>`
-      positionInfo = `Office Number: ${arr.officeNumber}`
+      roleInfo = `Office Number: ${arr.officeNumber}`
   } else if (arr.getPosition() === "Engineer") {
       positionIcon = `<i class="fas fa-glasses"></i>`
-      positionInfo = `GitHub Username: <a href="https://github.com/${arr.github}" target="_blank">${arr.github}</a>`
+      roleInfo = `GitHub Username: <a href="https://github.com/${arr.github}" target="_blank">${arr.github}</a>`
   } else if (arr.getPosition() === "Intern") {
       positionIcon = `<i class="fas fa-user-graduate"></i>`
-      positionInfo = `Institution: ${arr.school}`
+      roleInfo = `School: ${arr.school}`
   }
 
   return `
   
 <div class="col-md-4 col-sm-6 col-12 col-lg-3">
-    <div class="card shadow-lg mb-5 bg-white rounded">
-        <div class="card-header bg-primary">
+  <div class="card shadow-lg mb-5 bg-white rounded">
+      <div class="card-header bg-primary">
           <h4 class="text-white text-center">${arr.name}</h4>
           <h4 class="text-white text-center">${arr.getPosition()}</h4>
-        </div>
-        <div class="card-body">
+      </div>
+      <div class="card-body">
           <ul class="list-unstyled">
               <li>Employee ID: ${arr.id}</li>
               <li>Email: <a href="mailto:${arr.email}">${arr.email}</a></li>
-              <li>${positionInfo}</li>
+              <li>${roleInfo}</li>
           </u>
       </div>
   </div>
